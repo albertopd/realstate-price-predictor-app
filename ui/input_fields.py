42d022972b937654abe169c4e514d3ec0f541b04
Alberto <47937297+albertopd@users.noreply.github.com>
2025-07-09 10:04:43 +0200

    Improvements to input fields

--- ui/input_fields.py
+++ ui/input_fields.py
@@ -19,14 +19,6 @@ def collect_property_inputs():
     if subtype:
         data["subtype"] = subtype
 
-    epc = st.selectbox("EPC Score", [""] + [e.value for e in EPCScore])
-    if epc:
-        data["epcScore"] = epc
-
-    if (ts := st.slider("Terrace Surface (m²)", 0, 500)) > 0:
-        data["terraceSurface"] = ts
-    if (gs := st.slider("Garden Surface (m²)", 0, 2000)) > 0:
-        data["gardenSurface"] = gs
     if (b := st.slider("Bedrooms", 0, 10)) > 0:
         data["bedroomCount"] = b
     if (ba := st.slider("Bathrooms", 0, 10)) > 0:
@@ -34,12 +26,24 @@ def collect_property_inputs():
     if (t := st.slider("Toilets", 0, 10)) > 0:
         data["toiletCount"] = t
 
+    if (ts := st.slider("Terrace Surface (m²)", 0, 500)) > 0:
+        data["terraceSurface"] = ts
+        data["hasTerrace"] = True
+
+    if (gs := st.slider("Garden Surface (m²)", 0, 2000)) > 0:
+        data["gardenSurface"] = gs
+        data["hasGarden"] = True
+
+    epc = st.selectbox("EPC Score", [""] + [e.value for e in EPCScore])
+    if epc:
+        data["epcScore"] = epc
+
     st.markdown("#### Features:")
     features = [
         "hasAirConditioning", "hasArmoredDoor", "hasAttic", "hasBasement",
-        "hasDiningRoom", "hasDressingRoom", "hasFireplace", "hasGarden",
+        "hasDiningRoom", "hasDressingRoom", "hasFireplace",
         "hasHeatPump", "hasLift", "hasLivingRoom", "hasOffice",
-        "hasPhotovoltaicPanels", "hasSwimmingPool", "hasTerrace", "hasVisiophone"
+        "hasPhotovoltaicPanels", "hasSwimmingPool", "hasVisiophone"
     ]
     for f in features:
         data[f] = st.checkbox(add_space_before_caps(f.replace("has", "")), key=f)
